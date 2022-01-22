d0f671d10fc10302c40812a3dffff15cd3d9b236
MrPabloVS <pablovazquezsoldano@gmail.com>
2022-01-22 13:30:23 -0300

    crear router.js

---
+++ router.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const {Router} = express
+
+const router = new Router()
+
+router.get('/', (req, res) => {
+    res.send()
+})
+
+router.get('/:id', (req, res) => {
+    res.send()
+})
+
+router.post('/', (req, res) => {
+    res.send()
+})
+
+router.put('/:id', (req, res) => {
+    res.send()
+})
+
+router.delete('/:id', (req, res) => {
+    res.send()
+})
--- server.js
+++ server.js
@@ -3,28 +3,12 @@ const {Router} = express
 app.use(express.urlencoded({extended: true}))
 
 const app = express()
-const router = new Router()
 
-router.get('/', (req, res) => {
-    res.send()
-})
+const {router} = require('router.js')
 
-router.get('/:id', (req, res) => {
-    res.send()
-})
 
-router.post('/', (req, res) => {
-    res.send()
-})
-
-router.put('/:id', (req, res) => {
-    res.send()
-})
-
-router.delete('/:id', (req, res) => {
-    res.send()
-})
 
 app.use('/api/productos', router)
+app.use('/static', express.static(__dirname + '/public'))
 
 app.listen(8080)

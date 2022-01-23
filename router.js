f2f224ab6ea23c68ffb5ab9a098c15dc68af5320
MrPabloVS <pablovazquezsoldano@gmail.com>
2022-01-23 11:05:58 -0300

    primer intento de entrega

--- router.js
+++ router.js
@@ -1,24 +1,42 @@
 const express = require('express')
 const {Router} = express
+const multer = require('multer')
 
 const router = new Router()
 
+const fs = require("fs")
+const db = fs.readFileSync("productos.json")
+
+
 router.get('/', (req, res) => {
-    res.send()
+    res.send(db)
 })
 
-router.get('/:id', (req, res) => {
-    res.send()
+router.get('/:id', (err, req, res, next) => {
+    res.status(404).send('Producto no encontrado')
+    res.send(db.req.params)
 })
 
 router.post('/', (req, res) => {
-    res.send()
+    res.send({
+        title: nombreDelProducto,
+        price: 123,
+        img: '#',
+        id: db.length = 1
+    })
 })
 
-router.put('/:id', (req, res) => {
-    res.send()
+router.put('/:id', (err, req, res, next) => {
+    res.status(404).send('Producto no encontrado')
+    res.send({
+            title: nombreDelProducto,
+            price: 123,
+            img: '#',
+            id: req.params
+        })
 })
 
-router.delete('/:id', (req, res) => {
-    res.send()
+router.delete('/:id', (err, req, res, next) => {
+    res.status(404).send('Producto no encontrado')
+    res.send(`Producto de id: ${req.params} eliminado`)
 })
--- server.js
+++ server.js
@@ -1,14 +1,60 @@
 const express = require('express')
 const {Router} = express
 app.use(express.urlencoded({extended: true}))
+const {router} = require('router.js')
+const fs = require("fs")
 
 const app = express()
+const db = fs.readFileSync("productos.json")
+const usableId = db.length + 1
 
-const {router} = require('router.js')
+let newFileContent = []
 
 
 
+// Rutas
 app.use('/api/productos', router)
 app.use('/static', express.static(__dirname + '/public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
 
 app.listen(8080)
+
+// Set Storage
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, 'producto-' + usableId )
+    }
+})
+
+let upload = multer({storage: storage})
+
+//Funcion para actualizar productos.json
+function updateProducts() {
+    fs.readFile(`/uploads/producto${db.length}`, (err, contenido) => {
+        if(err){console.log('error al leer archivo')}
+        else{
+            newFileContent = contenido
+        }
+    })
+    fs.appendFile('/productos.json', newFileContent.json, err =>{
+        if(err){console.log('error al actualizar')}
+        else{console.log('actualizacion exitosa')}
+    })
+}
+
+// Subida de Archivo
+app.post('/uploadfile', upload.single('inputProducto'), (req, res, next) => {
+    const file = req.file
+    if(!file){
+        const error = new Error('Archivo no detectado')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.send(file)
+}).then(updateProducts())
+
